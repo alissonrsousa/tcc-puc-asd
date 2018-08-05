0e3597e77bf354a76160eb86a72a14b9bb5627e3
Alisson Sousa <correioalisson@gmail.com>
2018-08-04 21:14:33 -0300

    Auth server.

---
+++ code/auth-server/src/main/java/com/alissonrsousa/authserver/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.alissonrsousa.authserver.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alissonrsousa.authserver.model.Authority;
+
+public interface AuthorityRepository extends JpaRepository<Authority, String>{
+	
+	Authority findByName(String name);
+	
+}
