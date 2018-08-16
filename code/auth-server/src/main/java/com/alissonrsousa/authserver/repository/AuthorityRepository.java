0c41d8c87313f14b985fc78ab1573a551c3186c9
Alisson Sousa <correioalisson@gmail.com>
2018-08-16 19:56:10 -0300

    Ítens não usados

--- code/auth-server/src/main/java/com/alissonrsousa/authserver/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alissonrsousa.authserver.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.alissonrsousa.authserver.model.Authority;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String>{
-	Authority findByName(String name);
-}
