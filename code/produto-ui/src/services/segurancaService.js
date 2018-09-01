bca3cd83fc12ecbe2b63098dadfcd406a8ab312f
Alisson Sousa <correioalisson@gmail.com>
2018-09-01 15:24:37 -0300

    fornecedores

---
+++ code/produto-ui/src/services/segurancaService.js
@@ -0,0 +1,30 @@
+
+export default {
+
+	login () {
+	      var me = this
+	      var formData = new FormData();
+	      formData.append('username', me.usuario);
+	      formData.append('password', me.senha);
+	      formData.append('grant_type', 'password');
+
+	      Vue.http.post('http://localhost:9092/oauth/token', formData, {})
+	      .then((response) => {
+	          localStorage.setItem('authorization', JSON.stringify(response.body.access_token));
+
+//	           ir para a home do sistema
+	          location.href = '/';
+//	           this.$router.push({name: 'index'});
+	        }, err => {
+//	           console.log(err);
+	          alert('erro ao realizar o login');
+	        }
+	      );
+	},
+	    
+	sair () {
+	    localStorage.setItem('authorization', '');
+	    location.href = '#/login';
+	}
+
+}
